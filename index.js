8466e77e61c691bdf7f6d12da03f7b723954aa3c
lestercoyoy <lester_cch@hotmail.com>
2022-12-20 11:50:54 -0600

    ADD
    Connection to our DB. The parameters "username" and "password" were changed in order to avoid not wanted connections

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// REQUIRES
+var mongoose = require('mongoose');
+
+// PUERTO SERVIDOR
+var port = process.env.port || 3999;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.xalt6v5.mongodb.net/?retryWrites=true&w=majority',
+{useNewUrlParser: true})
+.then(
+    ()=>{
+        console.log('La conexion a la bd es correcta')
+        // CREAR EL SERVIDOR
+        append.lister(port,()=>{
+            console.log('El servidor http://localhost:3999 esta funcionando.')
+        });
+    }
+);

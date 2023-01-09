39631eb2acbfa5cd5f8c01d8a0a40236fcc8afbe
lestercoyoy <lester_cch@hotmail.com>
2023-01-09 11:51:57 -0600

    ADD
    Controller POC

---
+++ controller/product.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var client = require("../database/db");
+var db = client.db("testingbd");
+
+// CONTROLLERS
+var controller = {
+    // LIST
+
+    // SEARCH
+
+    // SAVE
+
+}
+
+module.exports = controller;

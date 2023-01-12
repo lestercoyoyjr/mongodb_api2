dd68973b6d5bfaa7e111ca74e5167f4ae5326579
lestercoyoy <lester_cch@hotmail.com>
2023-01-11 20:11:55 -0600

    ADD
    Routes to call our endpoints

--- app.js
+++ app.js
@@ -8,18 +8,22 @@ var bodyParser = require('body-parser');
 var app = express()
 
 // ROUTES LOADING FILES
+var product_routes = require('./routes/products');
 
 //MIDDLEWARES
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json);
 
-// REWRITE ROUTES
+// CORS AND HEADER SETTINGS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Rquested-With, Content-type, Accept, Access-Control-Allow-Request-Method');
     req.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     req.header('Allow', 'GET,M POST, OPTIONS, PUT, DELETE');
 });
+
+// REWRITE ROUTES
+app.use('/api', product_routes);
 
 // EXPORT MODULE
 module.exports = app;

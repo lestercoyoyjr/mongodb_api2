4ee42544a68e53f090491cb01c87fd198809b96f
lestercoyoy <lester_cch@hotmail.com>
2023-01-17 12:00:34 -0600

    MODIFY
    DB Connection

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 // REQUIRE
-var MongoClient = require('mongodb').MongoClient;
+var client = require('mongodb').MongoClient;
 // const {MongoCLient} = require("mongodb");
 require("dotenv").config();
 
@@ -7,13 +7,31 @@ require("dotenv").config();
 const DB = process.env.MONGODB_URI;
 // const client = new MongoCLient(DB);
 
-// CONNECTION]
-MongoClient.connect(DB, function(err, db) {
+// CONNECTION
+client.connect(DB, function(err, db) {
     if (err) {
         console.error(err);
     }
-    var collection = db.collection('collectionName');
+    var collection = db.collection('testingbd');
     collection.find().toArray(function(err, docs) {
         console.log(docs);
     });
 });
+
+module.exports = client;
+
+//////////////////////////////////////////////////////////////////////
+
+/* // const {MongoCLient} = require("mongodb");
+// const client = new MongoCLient(process.env.MONGODB_URI);
+
+// client.connect().then(
+//     (response) => {
+//         console.log('La conexion a la bd es correcta - url:' + response.url);
+//     },
+//     (error) => {
+//         console.log('error:' + error)
+//     }
+// )
+
+//  */
